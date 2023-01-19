827976c6ae5e016141ac7a048e4ad5700f3235e1
jlepoo <jana.lepojevic@hotmail.com>
2023-01-19 20:52:59 +1100

    import weather app

---
+++ src/weather.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function weather(props) {
+  if (props.temperature) {
+    return (
+      <div className="Weather">
+        <ul>
+          <li>Temperature: {Math.round(props.temperature)}°C</li>
+          <li>Description: {props.description}</li>
+          <li>Humidity: {props.humidity}%</li>
+          <li>Wind: {props.wind}km/h</li>
+          <li>
+            <img
+              src={`https://openweathermap.org/img/wn/${props.icon}@2x.png`}
+              height="100"
+              width="100"
+              alt={props.description}
+            />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
